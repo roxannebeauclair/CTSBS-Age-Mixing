fd68eed005486c1c019a1811a700e69618d1fc3f
roxannebeauclair <roxanne.beauclair@gmail.com>
2017-05-24 16:56:40 +0200

    Created separate script to amp models on bootstrap dfs

---
+++ Analysis/Scripts/08-Bootstrap.R
@@ -0,0 +1,79 @@
+# ========================================
+# Bootstrapping datasets and amp modelling 
+# ========================================
+
+# Author: Roxanne Beauclair
+
+# Description: This script takes the imputed datasets and 
+# bootstraps more datasets, then runs the amp models on them
+
+wd <- "/Users/roxannebeauclair/Documents/Analytical Projects/PhD/CTSbS Age Mixing/Analysis"
+cdata <- paste0(wd, "/Data/Cleaned")
+
+imputedata <- paste0(cdata, "/ctsbs_impute_data.rda")
+bootdata <- paste0(cdata, "/ctsbs_boot_amp_data.rda")
+
+fxn <- paste0(wd, "/Scripts/00-Functions.R")
+
+# ====================
+# Loading dependencies
+# ====================
+source(fxn)
+
+
+InstallLoad("magrittr", "tidyverse", 
+            "mice","nlme", "lme4", "modelr",
+            "mgcv", "broom", "splines")
+
+# =============
+# Load datasets
+# =============
+load(imputedata)
+
+
+# =========================
+# Run bootstrap then models
+# =========================
+
+# 1. Create a dataset that nests all of the datasets according to
+# gender, imputation, and hiv status.
+# 2. Bootstrap each of the 50 datasets for each subgroup
+# 3. Run regression models on all datasets
+# 4. Extract model components
+# 5. Output tidy df
+
+impsexhivamp <- dfimp %>%
+  filter(.imp != 0)%>%
+  group_by(.imp, sex, hiv) %>%
+  nest()
+
+start.time <- Sys.time()
+
+tidysumamp <- impsexhivamp %>%
+  mutate(model = pmap(list(data, as.character(sex), as.character(hiv)), # pmap changes sex and hiv to numeric, so must force character
+                      ampmodel), #ampmodel is function for nlme amp model
+         modelsum = map(model, ~tidy(.x, effects = "fixed")), # Obtaining all the b's and intercepts from the models
+         bvar = map(model, bvar), # Obtaining between subject variance using function bvar
+         wvar = map(model, wvar)) # Obtaining within subject variance using function wvar
+
+
+stop.time <- Sys.time()
+time <- stop.time - start.time
+time
+
+# ===================
+# Save tidy dataframe
+# ===================
+
+save(tidysumamp, file = bootdata)
+
+# ====================================================
+# Detach libraries and remove objects from environment
+# ====================================================
+Vectorize(detach)(name = paste0("package:", c("tidyverse", "mice","nlme", 
+                                              "lme4", "modelr",
+                                              "mgcv", "broom", "splines")), 
+                  unload = TRUE, 
+                  character.only = TRUE)
+
+rm(list=ls())

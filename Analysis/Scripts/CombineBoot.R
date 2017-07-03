572dbf9a32741725f2abe0107089060fd9d5a471
Roxanne Beauclair <roxannebeauclair@Roxannes-MacBook-Pro-2.local>
2017-07-03 10:42:36 +0200

    Added the script that combines all the ampmodel jobs done for each imputation

---
+++ Analysis/Scripts/CombineBoot.R
@@ -0,0 +1,53 @@
+# ========================================
+# Combining bootstrapped datasets 
+# ========================================
+
+# Author: Roxanne Beauclair
+
+# Description: This script takes all of the datasets
+# produced by the bootstrapping and models done in
+# the cluster, and combines them
+library(magrittr)
+library(tibble)
+library(tidyr)
+library(dplyr)
+
+# ==========
+# File paths
+# ==========
+wd <- "/user/data/gent/vsc400/vsc40070/CTSBS/"
+bdata <- paste0(wd, "data/2000reps/")
+
+bootdata <- paste0(bdata, "ctsbs_boot_rbind_data.rda")
+
+# ==================================
+# Load datasets and combine datasets
+# ==================================
+
+##initiate the big fat master dataset
+ampdf <- tibble(.imp = factor(),
+                            sex = factor(),
+                            hiv = factor(),
+                            .id = integer(),
+                            modelsum = list(),
+                            bvar = list(),
+                            wvar = list()) 
+
+for(i in 1:50){
+  path <- bdata
+  data.name <- paste0(path, "ctsbs_boot_amp_data_", i, ".rda")
+  
+  if(file.exists(data.name)) {
+    
+    load(data.name) # loading the tidysumamp object into the environment
+    
+    ampdf <- bind_rows(ampdf, tidysumamp)
+    
+  }
+}
+
+# ===========
+# Save tibble
+# ===========
+
+save(ampdf, file = bootdata)
